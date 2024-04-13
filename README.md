7904c420995021b0c1a0c73bc8ccf2cd376dcb0d
OptimisticNurul <OptimisticNurul@gmail.com>
2024-04-13 17:27:34 +0600

    OptimisticNurul

---
+++ README.md
@@ -0,0 +1,89 @@
+# School Management System Using CSharp, ADO.NET, SAP Crystal Reports
+
+The School Management System project is a desktop application developed using C# and ADO.NET for managing student and teacher data within a school environment. It provides functionalities such as adding, updating, and deleting student records, as well as viewing teacher information. The project utilizes Microsoft SQL Server for database management and SAP Crystal Reports for generating reports.
+
+## Project Structure
+
+The project consists of several C# classes organized into different sections:
+
+- **AddStudentData.cs:** Contains the class definition and methods for adding, updating, and deleting student records.
+- **AddTeachersData.cs:** Defines the class structure and functions for managing teacher information.
+- **Program.cs:** Main entry point for the application, responsible for initializing and running the user interface.
+
+## Features
+
+- **Student Data Management:** Allows users to add, update, and delete student records, including details such as student ID, name, gender, address, grade, section, image, status, and insertion date.
+- **Teacher Data Management:** Provides functionalities for managing teacher information, including teacher ID, name, gender, address, image, status, and insertion date.
+- **Database Connectivity:** Utilizes ADO.NET for connecting to the Microsoft SQL Server database and executing SQL queries.
+- **Dashboard Display:** Offers a dashboard view to display student records inserted on the current date.
+- **Report Generation:** Utilizes SAP Crystal Reports for generating various reports within the system.
+
+## Usage
+
+1. Ensure you have Visual Studio installed.
+2. Clone the repository to your local machine.
+3. Open the solution file in Visual Studio.
+4. Build the solution and resolve any dependencies.
+5. Set up the database connection string in the code if necessary.
+6. Run the application to access the student and teacher management functionalities.
+
+## Technologies Used
+
+- **C#:** Primary programming language used for development.
+- **ADO.NET:** Data access technology used for connecting to the SQL Server database and executing queries.
+- **Microsoft SQL Server:** Database management system utilized for storing student and teacher information.
+- **SAP Crystal Reports:** Utilized for generating reports within the system.
+
+## Database Backup
+
+The repository includes a backup file named `school.sql` containing the database schema and sample data. You can use this file to restore the database in your SQL Server instance.
+
+## Contributing
+
+Contributions to the project are welcome! If you have any suggestions, improvements, or bug fixes, feel free to fork the repository, make your changes, and submit a pull request.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE.txt).
+
+
+## Authors
+
+- [@OptimisticNurul](https://github.com/OptimisticNurul)
+
+
+# Hi, I'm Kazi Md Nurul Islam! 👋
+
+
+## 🚀 About Me
+
+I'm a Full Stack Developer, Graphic Designer, and Digital Marketer...
+
+🧠 Currently learning more...
+
+
+## 🛠 Skills
+
+As a Full Stack Developer:
+
+    C#, Microsoft Sql Server, Javascript, HTML, CSS, ASP.NET Core, ASP.NET Core API, Angular, React.js, MAUI...
+
+As a Graphic Designer:
+
+    Photoshop Design, illustration, Logo Design, Banner / Brochure / Flyer Design, Magazine / Book Cover Design, Business Card Design, Photo Retouch, Clipping Path, Background Removing, WaterMark Removing...
+
+As a Digital Marketer:
+
+    Search Engine Optimization (SEO), Social Media Marketing, Lead Generation, Email Marketing, Web Scraping...
+
+    
+## 🔗 Links
+
+[![portfolio](https://img.shields.io/badge/my_portfolio-000?style=for-the-badge&logo=ko-fi&logoColor=white)](https://github.com/OptimisticNurul/)
+
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/optimisticnurul/)
+
+
+## Support
+
+For support, email info.optimisticnurul@gmail.com or join our Slack channel.
---
+++ School Management System Using CSharp, ADO.NET, SAP Crystal Reports/SchoolMangementSystem/bin/Debug/AddStudentForm.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Drawing;
+
+
+namespace SchoolMangementSystem
+{
+    public partial class AddStudentForm : UserControl
+    {
+        SqlConnection connect = new SqlConnection(@"Data Source=DESKTOP-SNOOUCH\SQLEXPRESS;Initial Catalog=school;Integrated Security=True;");
+        SqlTransaction transaction;
+
+        public AddStudentForm()
+        {
+            InitializeComponent();
+            displayStudentData();
+        }
+
+        public void displayStudentData()
+        {
+            AddStudentData adData = new AddStudentData();
+            student_studentData.DataSource = adData.studentData();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (student_id.Text == "" || student_name.Text == "" || student_gender.Text == ""
+                || student_address.Text == "" || student_grade.Text == "" || student_section.Text == ""
+                || student_status.Text == "" || student_image.Image == null || imagePath == null)
+            {
+                MessageBox.Show("Please fill all blank fields", "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                if (connect.State != ConnectionState.Open)
+                {
+                    try
+                    {
+                        connect.Open();
+                        transaction = connect.BeginTransaction();
+
+                        string checkStudentID = "SELECT COUNT(*) FROM students WHERE student_id = @studentID";
+
+                        using (SqlCommand checkSID = new SqlCommand(checkStudentID, connect, transaction))
+                        {
+                            checkSID.Parameters.AddWithValue("@studentID", student_id.Text.Trim());
+                            int count = (int)checkSID.ExecuteScalar();
+
+                            if (count >= 1)
+                            {
+                                MessageBox.Show("Student ID: " + student_id.Text.Trim() + " is already exist"
+                                    , "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                transaction.Rollback();
+                                return;
+                            }
+                        }
+
+                        DateTime today = DateTime.Today;
+                        string insertData = "INSERT INTO students (student_id, student_name" +
+                            ", student_gender, student_address, student_grade, student_section" +
+                            ", student_image, student_status, date_insert) " +
+                            "VALUES(@studentID, @studentName, @studentGender, @studentAddress" +
+                            ", @studentGrade, @studentSection, @studentImage, @studentStatus" +
+                            ", @dateInsert)";
+
+                        string path = Path.Combine(@"..\..\Student_Directory\", student_id.Text.Trim() + ".jpg");
+                        string directoryPath = Path.GetDirectoryName(path);
+
+                        if (!Directory.Exists(directoryPath))
+                        {
+                            Directory.CreateDirectory(directoryPath);
+                        }
+
+                        File.Copy(imagePath, path, true);
+
+                        using (SqlCommand cmd = new SqlCommand(insertData, connect, transaction))
+                        {
+                            cmd.Parameters.AddWithValue("@studentID", student_id.Text.Trim());
+                            cmd.Parameters.AddWithValue("@studentName", student_name.Text.Trim());
+                            cmd.Parameters.AddWithValue("@studentGender", student_gender.Text.Trim());
+                            cmd.Parameters.AddWithValue("@studentAddress", student_address.Text.Trim());
+                            cmd.Parameters.AddWithValue("@studentGrade", student_grade.Text.Trim());
+                            cmd.Parameters.AddWithValue("@studentSection", student_section.Text.Trim());
+                            cmd.Parameters.AddWithValue("@studentImage", path);
+                            cmd.Parameters.AddWithValue("@studentStatus", student_status.Text.Trim());
+                            cmd.Parameters.AddWithValue("@dateInsert", today.ToString());
+
+                            cmd.ExecuteNonQuery();
+                        }
+
+                        transaction.Commit();
+
+                        displayStudentData();
+
+                        MessageBox.Show("Added successfully!", "Information Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                        clearFields();
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        MessageBox.Show("Error connecting Database: " + ex, "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    finally
+                    {
+                        connect.Close();
+                    }
+                }
+            }
+        }
+
+        public void clearFields()
+        {
+            student_id.Text = "";
+            student_name.Text = "";
+            student_gender.SelectedIndex = -1;
+            student_address.Text = "";
+            student_grade.SelectedIndex = -1;
+            student_section.SelectedIndex = -1;
+            student_status.SelectedIndex = -1;
+            student_image.Image = null;
+            imagePath = "";
+        }
+
+        public string imagePath;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "Image files (*.jpg; *.png)|*.jpg;*.png";
+
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                imagePath = open.FileName;
+
+                student_image.ImageLocation = imagePath;
+            }
+        }
+
+        private void student_updateBtn_Click(object sender, EventArgs e)
+        {
+            if (student_id.Text == "" || student_name.Text == "" || student_gender.Text == ""
+                || student_address.Text == "" || student_grade.Text == "" || student_section.Text == ""
+                || student_status.Text == "" || student_image.Image == null || imagePath == null)
+            {
+                MessageBox.Show("Please select item first", "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                if (connect.State != ConnectionState.Open)
+                {
+                    try
+                    {
+                        connect.Open();
+                        transaction = connect.BeginTransaction();
+
+                        DialogResult check = MessageBox.Show("Are you sure you want to Update Student ID: "
+                            + student_id.Text.Trim() + "?", "Confirmation Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                        if (check == DialogResult.Yes)
+                        {
+                            DateTime today = DateTime.Today;
+
+                            String updateData = "UPDATE students SET student_name = @studentName, " +
+                                "student_gender = @studentGender, student_address = @studentAddress, " +
+                                "student_grade = @studentGrade, student_section = @studentSection, " +
+                                "student_status = @studentStatus, date_update = @dateUpdate " +
+                                "WHERE student_id = @studentID";
+
+                            using (SqlCommand cmd = new SqlCommand(updateData, connect, transaction))
+                            {
+                                cmd.Parameters.AddWithValue("@studentName", student_name.Text.Trim());
+                                cmd.Parameters.AddWithValue("@studentGender", student_gender.Text.Trim());
+                                cmd.Parameters.AddWithValue("@studentAddress", student_address.Text.Trim());
+                                cmd.Parameters.AddWithValue("@studentGrade", student_grade.Text.Trim());
+                                cmd.Parameters.AddWithValue("@studentSection", student_section.Text.Trim());
+                                cmd.Parameters.AddWithValue("@studentStatus", student_status.Text.Trim());
+                                cmd.Parameters.AddWithValue("@dateUpdate", today.ToString());
+                                cmd.Parameters.AddWithValue("@studentID", student_id.Text.Trim());
+
+                                cmd.ExecuteNonQuery();
+                            }
+
+                            transaction.Commit();
+
+                            displayStudentData();
+
+                            MessageBox.Show("Updated successfully!", "Information Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                            clearFields();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Cancelled.", "Information Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            clearFields();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        MessageBox.Show("Error connecting Database: " + ex, "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    finally
+                    {
+                        connect.Close();
+                    }
+                }
+            }
+        }
+
+        private void student_studentData_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex != -1)
+            {
+                DataGridViewRow row = student_studentData.Rows[e.RowIndex];
+                student_id.Text = row.Cells[1].Value.ToString();
+                student_name.Text = row.Cells[2].Value.ToString();
+                student_gender.Text = row.Cells[3].Value.ToString();
+                student_address.Text = row.Cells[4].Value.ToString();
+                student_grade.Text = row.Cells[5].Value.ToString();
+                student_section.Text = row.Cells[6].Value.ToString();
+
+                imagePath = row.Cells[5].Value.ToString();
+
+                string imageData = row.Cells[7].Value.ToString();
+
+                if (imageData != null && imageData.Length > 0)
+                {
+                    student_image.Image = Image.FromFile(imageData);
+                }
+                else
+                {
+                    student_image.Image = null;
+                }
+
+                student_status.Text = row.Cells[8].Value.ToString();
+
+            }
+        }
+
+        private void student_deleteBtn_Click(object sender, EventArgs e)
+        {
+            if (student_id.Text == "")
+            {
+                MessageBox.Show("Please select item first", "Error Message"
+                    , MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                if (connect.State != ConnectionState.Open)
+                {
+                    DialogResult check = MessageBox.Show("Are you sure you want to Delete Student ID: "
+                        + student_id.Text + "?", "Confirmation Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                    if (check == DialogResult.Yes)
+                    {
+
+                        try
+                        {
+                            connect.Open();
+                            DateTime today = DateTime.Today;
+
+                            string deleteData = "UPDATE students SET date_delete = @dateDelete " +
+                                "WHERE student_id = @studentID";
+
+                            using (SqlCommand cmd = new SqlCommand(deleteData, connect))
+                            {
+                                cmd.Parameters.AddWithValue("@dateDelete", today.ToString());
+                                cmd.Parameters.AddWithValue("@studentID", student_id.Text.Trim());
+
+                                cmd.ExecuteNonQuery();
+
+                                displayStudentData();
+
+                                MessageBox.Show("Deleted successfully!", "Information Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                                clearFields();
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Error connecting Database: " + ex, "Error Message"
+                        , MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        finally
+                        {
+                            connect.Close();
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Cancelled.", "Information Message"
+                        , MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+
+                }
+            }
+        }
+
+        private void student_clearBtn_Click(object sender, EventArgs e)
+        {
+            clearFields();
+        }
+    }
+}
